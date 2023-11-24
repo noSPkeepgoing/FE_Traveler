79c437bf3e0acc7e303df374922eb0a19d84b802
junkue20 <junkue13@gmail.com>
2023-11-24 12:51:01 +0900

    🎨 Style: header 버튼 Button 컴포넌트로 스타일 수정

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,8 +1,15 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
 import styles from './header.module.scss';
 import React from 'react';
+import Button from '../button';
+import Text from '../text';
 
 function header() {
-  const isOnline = true;
+  const isOnline = false;
+  const router = useRouter();
+
   return (
     <div className={styles.body}>
       <div className={styles.container}>
@@ -10,22 +17,34 @@ function header() {
         <div className={styles.buttons}>
           {!isOnline ? (
             <>
-              <a className={styles['buttons-child']} href="/sign-in">
-                로그인
-              </a>
-              <a className={styles['buttons-child']} href="/sign-up">
-                회원가입
-              </a>
+              <div className={`${styles.buttons} ${styles.child}`}>
+                <Button variant="text" onClick={() => router.push('/sign-in')}>
+                  <Text fontSize="xs" fontWeight="normal">
+                    로그인
+                  </Text>
+                </Button>
+              </div>
+              <div className={`${styles.buttons} ${styles.child}`}>
+                <Button variant="text" onClick={() => router.push('/sign-up')}>
+                  <Text fontSize="xs" fontWeight="normal">
+                    회원가입
+                  </Text>
+                </Button>
+              </div>
             </>
           ) : (
             <>
-              <a className={styles['buttons-child']} href="/main">
+              <a className={`${styles.buttons} ${styles.child}`} href="/main">
                 로그아웃
               </a>
-              <a className={styles['buttons-child']} href="/reservation-list">
+              <a
+                className={`${styles.buttons} ${styles.child}`}
+                href="/reservation-list">
                 장바구니
               </a>
-              <a className={styles['buttons-child']} href="/reservation-check">
+              <a
+                className={`${styles.buttons} ${styles.child}`}
+                href="/reservation-check">
                 주문내역
               </a>
             </>
