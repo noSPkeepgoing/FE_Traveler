023cf76f98f1dd52ea7d66627ec9f6dabbd2df8d
junkue20 <junkue13@gmail.com>
2023-11-25 20:43:07 +0900

    ♻️ Refactor: normalize.scss 주석처리

--- src/app/sign-up/layout.tsx
+++ src/app/sign-up/layout.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from './signUp.module.scss';
-import '../../styles/_normalize.scss';
+// import '@styles/_normalize.scss';
 
 function layout({ children }: { children: React.ReactNode }) {
   return <main className={styles.frame}>{children}</main>;
