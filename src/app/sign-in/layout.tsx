236fca63b6a4f46b0f4d9db2dca9ac078e5d7b33
junkue20 <junkue13@gmail.com>
2023-11-26 14:52:39 +0900

    📦 Chore: dev브랜치 pull  용 커밋 작성

--- src/app/sign-in/layout.tsx
+++ src/app/sign-in/layout.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-// import '@styles/_normalize.scss';
+// import '@styles/_normalize.scss'
 
 function SignInLayout({ children }: { children: React.ReactNode }) {
   return <main>{children}</main>;
