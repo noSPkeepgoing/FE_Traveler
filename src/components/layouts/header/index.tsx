15737c40c75f1c677683631c08e8f085816547c6
junkue20 <junkue13@gmail.com>
2023-11-26 21:46:18 +0900

    ♻️ Refactor: Header props 타입 지정 및 폰트 크기 조정

--- src/components/layouts/header/index.tsx
+++ src/components/layouts/header/index.tsx
@@ -2,65 +2,79 @@
 
 import { useRouter } from 'next/navigation';
 import styles from './header.module.scss';
+import classNames from 'classnames';
 import React from 'react';
 import Button from '../../atoms/button';
 import Text from '../../atoms/text';
+import { THeader } from './headerType';
 
-function header() {
-  const isOnline = false;
+function Header({ border = true }: THeader) {
+  const containerClassName = classNames(styles.container, {
+    [styles.border]: border,
+  });
+
+  const isOnline = true;
   const router = useRouter();
 
   return (
-    <div className={styles.container}>
-      <div className={styles.logo}>LOGO</div>
-      <div className={styles.buttons}>
-        {!isOnline ? (
-          <>
-            <div className={`${styles.buttons} ${styles.child}`}>
-              <Button variant="text" onClick={() => router.push('/sign-in')}>
-                <Text fontSize="xs" fontWeight="normal">
-                  로그인
-                </Text>
-              </Button>
-            </div>
-            <div className={`${styles.buttons} ${styles.child}`}>
-              <Button variant="text" onClick={() => router.push('/sign-up')}>
-                <Text fontSize="xs" fontWeight="normal">
-                  회원가입
-                </Text>
-              </Button>
-            </div>
-          </>
-        ) : (
-          <>
-            <div className={`${styles.buttons} ${styles.child}`}>
-              <Button variant="text" onClick={() => router.push('/sign-in')}>
-                <Text fontSize="xs" fontWeight="normal">
-                  로그아웃
-                </Text>
-              </Button>
-            </div>
-            <div className={`${styles.buttons} ${styles.child}`}>
-              <Button variant="text" onClick={() => router.push('/cart')}>
-                <Text fontSize="xs" fontWeight="normal">
-                  장바구니
-                </Text>
-              </Button>
-            </div>
-            <div className={`${styles.buttons} ${styles.child}`}>
-              <Button
-                variant="text"
-                onClick={() => router.push('/reservation-list')}>
-                <Text fontSize="xs" fontWeight="normal">
-                  주문내역
-                </Text>
-              </Button>
-            </div>
-          </>
-        )}
+    <div className={containerClassName}>
+      <div className={styles.container}>
+        <div className={styles.logo}>
+          <Button variant="text" onClick={() => router.push('/main')}>
+            <Text fontSize="xl" fontWeight="thin">
+              LOGO
+            </Text>
+          </Button>
+        </div>
+        <div className={styles.buttons}>
+          {!isOnline ? (
+            <>
+              <div className={`${styles.buttons} ${styles.child}`}>
+                <Button variant="text" onClick={() => router.push('/sign-in')}>
+                  <Text fontSize="xs-2" fontWeight="semibold">
+                    로그인
+                  </Text>
+                </Button>
+              </div>
+              <div className={`${styles.buttons} ${styles.child}`}>
+                <Button variant="text" onClick={() => router.push('/sign-up')}>
+                  <Text fontSize="xs-2" fontWeight="semibold">
+                    회원가입
+                  </Text>
+                </Button>
+              </div>
+            </>
+          ) : (
+            <>
+              <div className={`${styles.buttons} ${styles.child}`}>
+                <Button variant="text" onClick={() => router.push('/sign-in')}>
+                  <Text fontSize="xs-2" fontWeight="semibold">
+                    로그아웃
+                  </Text>
+                </Button>
+              </div>
+              <div className={`${styles.buttons} ${styles.child}`}>
+                <Button variant="text" onClick={() => router.push('/cart')}>
+                  <Text fontSize="xs-2" fontWeight="semibold">
+                    장바구니
+                  </Text>
+                </Button>
+              </div>
+              <div className={`${styles.buttons} ${styles.child}`}>
+                <Button
+                  variant="text"
+                  onClick={() => router.push('/reservation-list')}>
+                  <Text fontSize="xs-2" fontWeight="semibold">
+                    주문내역
+                  </Text>
+                </Button>
+              </div>
+            </>
+          )}
+        </div>
       </div>
     </div>
   );
 }
 
-export default header;
+export default Header;
