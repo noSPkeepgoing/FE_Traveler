0ff2a0db4d859ba142719bdc9bf89da6fafc056d
junkue20 <junkue13@gmail.com>
2023-11-26 14:54:01 +0900

    📦 Chore: dev브랜치 pull용 커밋 작성

--- src/app/sign-up/layout.tsx
+++ src/app/sign-up/layout.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from './signUp.module.scss';
-// import '@styles/_normalize.scss';
+// import '@styles/_normalize.scss'
 
 function SignUpLayout({ children }: { children: React.ReactNode }) {
   return <main className={styles.frame}>{children}</main>;
