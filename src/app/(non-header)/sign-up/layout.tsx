bb42111f2f885bd8a378b01267eba9f480f35e74
junkue20 <junkue13@gmail.com>
2023-11-27 12:47:30 +0900

    🎨 Style: layout 스타일 클래스명 변경

--- src/app/(non-header)/sign-up/layout.tsx
+++ src/app/(non-header)/sign-up/layout.tsx
@@ -3,7 +3,7 @@ import styles from './signUp.module.scss';
 // import '@styles/_normalize.scss'
 
 function SignUpLayout({ children }: { children: React.ReactNode }) {
-  return <main className={styles.frame}>{children}</main>;
+  return <main className={styles.container}>{children}</main>;
 }
 
 export default SignUpLayout;
