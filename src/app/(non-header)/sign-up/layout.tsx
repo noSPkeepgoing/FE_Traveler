c2b99e60bb28cea2561599799410755572e2d7c7
junkue20 <junkue13@gmail.com>
2023-11-27 15:38:39 +0900

    📦 Chore: 로그인, 회원가입 layout 통합 및 불필요 주석 삭제처리

--- src/app/(non-header)/sign-up/layout.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import styles from './signUp.module.scss';
-// import '@styles/_normalize.scss'
-
-function SignUpLayout({ children }: { children: React.ReactNode }) {
-  return <main className={styles.container}>{children}</main>;
-}
-
-export default SignUpLayout;
