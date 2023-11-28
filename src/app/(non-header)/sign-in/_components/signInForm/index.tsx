02d98f9678e4fd39f0445c1631cefac3a128b908
junkue20 <junkue13@gmail.com>
2023-11-29 01:47:16 +0900

    ♻️ Refactor: 로그인 페이지 버튼 타입 변경

--- src/app/(non-header)/sign-in/_components/signInForm/index.tsx
+++ src/app/(non-header)/sign-in/_components/signInForm/index.tsx
@@ -14,18 +14,6 @@ function SignInForm() {
     // 폼 제출(submit) 이벤트 발생 시 실행할 api가 들어갈 자리
   };
 
-  // form 제출 시 유효성 검사용 함수
-  // const MyFormComponent: React.FC = () => {
-  //   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-  //     // 이벤트 핸들러에서 폼 데이터를 수집하거나 사용할 수 있도록 로직 추가
-  //     const formData: TSignIn = {
-  //       email: 'user@example.com',
-  //       password: '1q2w3e4r!',
-  //     };
-
-  //     signUpSubmit(event, formData);
-  //   };
-  // };
   return (
     <form action="/v1/member/login" method="post">
       <div className={styles.form}>
@@ -56,7 +44,7 @@ function SignInForm() {
           </Button>
         </div>
         <div className={styles.signInBtn}>
-          <Button type="Link" variant="text" href="/sign-up">
+          <Button type="button" variant="text" href="/sign-up">
             <Text fontSize="xs-3" fontWeight="bold">
               아직 회원이 아니신가요?
             </Text>
