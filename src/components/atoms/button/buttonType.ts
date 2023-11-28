db08dd8f210b3a96e7004bb1387e4217bae6f275
junkue20 <junkue13@gmail.com>
2023-11-28 21:56:27 +0900

    ♻️ Refactor: Button 컴포넌트 onClick 속성 함수값 변경

--- src/components/atoms/button/buttonType.ts
+++ src/components/atoms/button/buttonType.ts
@@ -2,7 +2,7 @@ export type TButton = {
   size?: 'lg' | 'xl' | 'md' | 'sm' | '';
   variant?: 'default' | 'text' | 'secondary';
   children?: React.ReactNode;
-  onClick?: () => void;
+  onClick?: (v: unknown) => unknown;
   disabled?: boolean;
   type?: 'button' | 'submit' | 'reset';
   href?: string;
