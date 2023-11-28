a89c86065ae4cae82ba05bb19738fb6f06dd294a
89882 <pgh123166@naver.com>
2023-11-28 15:55:47 +0900

    ✨Feat : React-query provider 설정

---
+++ src/AppProvider.tsx
@@ -0,0 +1,24 @@
+'use client';
+import { ReactNode, useState } from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+export const AppProvider = ({ children }: { children: ReactNode }) => {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            refetchOnWindowFocus: false,
+            staleTime: 1000 * 20,
+          },
+        },
+      }),
+  );
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  );
+};
