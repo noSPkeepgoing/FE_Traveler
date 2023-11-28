ed2e525f57ea49c32b6888a530b25ea9b6f0df0d
junkue20 <junkue13@gmail.com>
2023-11-29 01:45:54 +0900

    ✨ Feat: 로그인 API 작성 (작업중)

--- src/api/signIn/Index.ts
+++ src/api/signIn/Index.ts
@@ -1,1 +1,26 @@
-export const SIGNIN_API = {};
+import { isAxiosError } from 'axios';
+import { instance } from '../';
+import { Response, ErrorData } from '../type';
+import { TSignInData } from './signInType';
+
+export const SIGNIN_API = {
+  userSignIn: async (userData: TSignInData) => {
+    try {
+      const response = await instance.post('/v1/member/login', userData);
+      const code = response.data.code;
+      const accessToken = response.data.data.accessToken;
+      const refreshtoken = response.data.data.refreshtoken;
+      // 1. 쿠키에 넣기(2개 다! 원래는 따로.)
+
+      // 2. accessToken 주기에 맞춰서 쿠키에 넣기.
+      return code;
+    } catch (error) {
+      if (isAxiosError<Response<ErrorData>>(error)) {
+        if (error.response) {
+          const code: number = error.response.data.code;
+          return code;
+        }
+      }
+    }
+  },
+};
