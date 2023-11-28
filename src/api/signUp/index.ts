b6139f686992a34b872c4c4f90b7e22d7bb11dd4
junkue20 <junkue13@gmail.com>
2023-11-28 18:30:07 +0900

    ♻️ Refactor: api 함수 수정

--- src/api/signUp/index.ts
+++ src/api/signUp/index.ts
@@ -1,28 +1,29 @@
 import { instance } from '../';
 import { TSignUpData, TResponse } from './signUpType';
+import { isAxiosError } from 'axios';
 
 export const SIGN_API = {
   // 회원가입 API
-  userSignUp: async (userData: TSignUpData): Promise<TResponse> => {
-    try {
-      const response = await instance.post('/v1/member/register', userData);
-      const { code, data } = response.data;
-      return { code, data };
-    } catch (error) {
-      throw new Error('서버와 통신중 에러가 발생');
-    }
+  userSignUp: async (userData: TSignUpData) => {
+    const response = await instance.post('/v1/member/register', userData);
+    const { code, data } = response.data;
+    return { code, data };
   },
 
   // 이메일 중복체크 API
-  emailCheck: async (email: string): Promise<TResponse> => {
+  emailCheck: async (email: string) => {
     try {
       const response = await instance.get(
         `v1/member/register/check?email=${email}`,
       );
+      console.log('respose: ', response);
       const { code, data } = response.data;
       return { code, data };
-    } catch (error) {
-      throw new Error('서버와 통신중 에러가 발생');
+    } catch (error: unknown) {
+      if (isAxiosError<TResponse>(error)) {
+        const { code, data } = error.response!.data;
+        return { code, data };
+      }
     }
   },
 };
