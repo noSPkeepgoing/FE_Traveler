1c31fa286454075a3cbf9e22aa483fae46eceb47
89882 <pgh123166@naver.com>
2023-11-28 22:35:18 +0900

    📦 Chore: 임시 토큰 추가

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,7 +3,10 @@ import axios from 'axios';
 
 export const instance = axios.create({
   baseURL: HTTP_BASE_URL,
-  headers: {},
+  headers: {
+    Authorization:
+      'Bearer eyJhbGciOiJIUzUxMiJ9.eyJjbGllbnRfaWQiOiIyIiwiZW1haWwiOiJ0ZXN0QGdtYWlsLmNvbSIsImlzcyI6ImZjX21pbmkiLCJpYXQiOjE3MDExNjM3MzYsImV4cCI6MTcwMTE5OTczNn0.fSDhmD8alzdyXK64obHEp-CSclULpWWnT2IPlH2Cu_k8QPdXcPEGlN3ptoFkJnXFdNjxgJLQQeApLZeVy27tVQ',
+  },
 });
 
 // interceptor 코드
