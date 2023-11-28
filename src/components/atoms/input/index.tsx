e9d8d835226a3e710a69a0445d80e79a217862d5
Diana Seo <syb0127@berkeley.edu>
2023-11-28 18:11:40 +0900

    Feat: Input 타입 추가

--- src/components/atoms/input/index.tsx
+++ src/components/atoms/input/index.tsx
@@ -1,14 +1,25 @@
 import React, { useState } from 'react';
 import classNames from 'classnames';
 import { TInput } from './inputType';
-import './input.scss';
+import styles from './input.module.scss';
 
-function Input({ variant, placeholder }: TInput) {
+function Input({ variant, type, placeholder, invalid }: TInput) {
   const inputPlaceholder = placeholder ?? '';
 
-  const className = classNames('input', variant);
+  const inputClasses = classNames('input', {
+    [styles.invalid]: invalid,
+    [styles.login]: variant === 'login',
+    [styles.signUpShort]: variant === 'signUpShort',
+    [styles.reservation]: variant === 'reservation',
+  });
 
-  return <input className={className} placeholder={inputPlaceholder} />;
+  return (
+    <input
+      className={inputClasses}
+      type={type}
+      placeholder={inputPlaceholder}
+    />
+  );
 }
 
 export default Input;
--- src/components/atoms/input/inputType.ts
+++ src/components/atoms/input/inputType.ts
@@ -1,4 +1,6 @@
 export type TInput = {
   variant?: 'signUpShort' | 'login' | 'reservation';
+  type?: 'text' | 'email' | 'password';
   placeholder?: string;
+  invalid?: boolean;
 };
