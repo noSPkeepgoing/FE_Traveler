fc79083537a79ee60251245fe54454e4c26a7290
junkue20 <junkue13@gmail.com>
2023-11-29 22:33:34 +0900

    ♻️ Refactor: api용 타입 (토큰 및 사용자 정보 타입) 추가

--- src/api/signIn/signInType.ts
+++ src/api/signIn/signInType.ts
@@ -2,3 +2,10 @@ export type TSignInData = {
   email: string;
   password: string;
 };
+
+export type TSignInResponseData = {
+  access_token: string;
+  refresh_token: string;
+  name: string;
+  email: string;
+};
