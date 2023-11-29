55455c93238614ec3838195059ce3448f3446375
junkue20 <junkue13@gmail.com>
2023-11-29 19:20:37 +0900

    📦 Chore: api 코드 줄 제거

--- src/api/signUp/index.ts
+++ src/api/signUp/index.ts
@@ -7,7 +7,6 @@ export const SIGN_API = {
   // 회원가입 API 수정용
   userSignUp: (userData: TSignUpData) =>
     instance.post<Response>('v1/member/register', userData),
-
   // 이메일 중복체크 api
   emailCheck: async (email: string) =>
     instance.get<Response>(`v1/member/register/check?email=${email}`),
