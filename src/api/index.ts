f55374fef0986c5678b9ea88b9c3728cf0996cce
junkue20 <junkue13@gmail.com>
2023-11-30 03:41:56 +0900

    ♻️ Refactor: 요청 인터셉터 추가

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,12 +2,26 @@ import { HTTP_BASE_URL } from '@/constants/api';
 import axios from 'axios';
 
 const accessToken = sessionStorage.getItem('access_token');
+const headers = accessToken ? { Authorization: `Bearer ${accessToken}` } : {};
 
 export const instance = axios.create({
   baseURL: HTTP_BASE_URL,
-  headers: {
-    Authorization: `Bearer ${accessToken}`,
-  },
+  headers: headers,
 });
 
-// interceptor 코드
+// 요청 인터셉터 설정
+instance.interceptors.request.use(
+  (config) => {
+    const accessToken = sessionStorage.getItem('access_token'); // 세션 스토리지에서 accessToken 가져오기
+
+    // accessToken이 존재하면 Authorization 헤더 설정
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
