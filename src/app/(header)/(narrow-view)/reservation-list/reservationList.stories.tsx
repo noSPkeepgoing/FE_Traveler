eb80ae86b403094a6230204f251ee92f0691e5ba
89882 <pgh123166@naver.com>
2023-11-30 03:03:39 +0900

    📦 Chore: 스토리북 코드 수정

--- src/app/(header)/(narrow-view)/reservation-list/reservationList.stories.tsx
+++ src/app/(header)/(narrow-view)/reservation-list/reservationList.stories.tsx
@@ -37,10 +37,206 @@ export const ExampleReservationList: StoryObj = {
     },
   },
   render: () => {
-    const [items, setItems] = useState([0, 1, 2, 3, 4, 5, 6]);
+    const [items, setItems] = useState([
+      {
+        order_date: '2023-11-29',
+        accommodation_id: 104,
+        accommodation_name: '킨텍스 바이 케이트리',
+        accommodation_img:
+          'http://tong.visitkorea.or.kr/cms/resource/12/2872912_image2_1.jpeg',
+        people_number: 2,
+        start_date: '2023-12-03',
+        end_date: '2023-12-04',
+        representative_name: '홍길동',
+        order_price: 440000,
+      },
+      {
+        order_date: '2023-11-29',
+        accommodation_id: 103,
+        accommodation_name: '라마다앙코르 바이윈덤 김포한강호텔',
+        accommodation_img:
+          'http://tong.visitkorea.or.kr/cms/resource/76/2874676_image2_1.jpg',
+        people_number: 2,
+        start_date: '2023-12-03',
+        end_date: '2023-12-04',
+        representative_name: '홍길동',
+        order_price: 280000,
+      },
+      {
+        order_date: '2023-11-29',
+        accommodation_id: 102,
+        accommodation_name: '호텔M-Tower',
+        accommodation_img:
+          'http://tong.visitkorea.or.kr/cms/resource/05/2874705_image2_1.jpg',
+        people_number: 2,
+        start_date: '2023-12-03',
+        end_date: '2023-12-04',
+        representative_name: '홍길동',
+        order_price: 60000,
+      },
+      {
+        order_date: '2023-11-29',
+        accommodation_id: 101,
+        accommodation_name: '호텔오로라',
+        accommodation_img:
+          'http://tong.visitkorea.or.kr/cms/resource/36/2874836_image2_1.jpg',
+        people_number: 2,
+        start_date: '2023-12-03',
+        end_date: '2023-12-04',
+        representative_name: '홍길동',
+        order_price: 70000,
+      },
+      {
+        order_date: '2023-11-29',
+        accommodation_id: 100,
+        accommodation_name: '저스트슬립호텔 김포구래1호점',
+        accommodation_img:
+          'http://tong.visitkorea.or.kr/cms/resource/83/2874883_image2_1.jpg',
+        people_number: 2,
+        start_date: '2023-12-03',
+        end_date: '2023-12-04',
+        representative_name: '홍길동',
+        order_price: 50000,
+      },
+      {
+        order_date: '2023-11-29',
+        accommodation_id: 99,
+        accommodation_name: '호텔 라르 김포',
+        accommodation_img:
+          'http://tong.visitkorea.or.kr/cms/resource/04/2874904_image2_1.jpg',
+        people_number: 2,
+        start_date: '2023-12-03',
+        end_date: '2023-12-04',
+        representative_name: '홍길동',
+        order_price: 700000,
+      },
+      {
+        order_date: '2023-11-29',
+        accommodation_id: 96,
+        accommodation_name: '루브호텔',
+        accommodation_img:
+          'http://tong.visitkorea.or.kr/cms/resource/91/2877391_image2_1.jpg',
+        people_number: 2,
+        start_date: '2023-12-03',
+        end_date: '2023-12-04',
+        representative_name: '홍길동',
+        order_price: 40000,
+      },
+      {
+        order_date: '2023-11-29',
+        accommodation_id: 95,
+        accommodation_name: '라인플러스호텔',
+        accommodation_img:
+          'http://tong.visitkorea.or.kr/cms/resource/25/2877425_image2_1.jpg',
+        people_number: 2,
+        start_date: '2023-12-03',
+        end_date: '2023-12-04',
+        representative_name: '홍길동',
+        order_price: 60000,
+      },
+    ]);
     const fetchMoreData = () => {
-      // 새로운 데이터를 가져와서 state 업데이트
-      setItems((prev) => [...prev, 0, 1, 2, 3, 4, 5, 6]);
+      setItems((prev) => [
+        ...prev,
+        ...[
+          {
+            order_date: '2023-11-29',
+            accommodation_id: 104,
+            accommodation_name: '킨텍스 바이 케이트리',
+            accommodation_img:
+              'http://tong.visitkorea.or.kr/cms/resource/12/2872912_image2_1.jpeg',
+            people_number: 2,
+            start_date: '2023-12-03',
+            end_date: '2023-12-04',
+            representative_name: '홍길동',
+            order_price: 440000,
+          },
+          {
+            order_date: '2023-11-29',
+            accommodation_id: 103,
+            accommodation_name: '라마다앙코르 바이윈덤 김포한강호텔',
+            accommodation_img:
+              'http://tong.visitkorea.or.kr/cms/resource/76/2874676_image2_1.jpg',
+            people_number: 2,
+            start_date: '2023-12-03',
+            end_date: '2023-12-04',
+            representative_name: '홍길동',
+            order_price: 280000,
+          },
+          {
+            order_date: '2023-11-29',
+            accommodation_id: 102,
+            accommodation_name: '호텔M-Tower',
+            accommodation_img:
+              'http://tong.visitkorea.or.kr/cms/resource/05/2874705_image2_1.jpg',
+            people_number: 2,
+            start_date: '2023-12-03',
+            end_date: '2023-12-04',
+            representative_name: '홍길동',
+            order_price: 60000,
+          },
+          {
+            order_date: '2023-11-29',
+            accommodation_id: 101,
+            accommodation_name: '호텔오로라',
+            accommodation_img:
+              'http://tong.visitkorea.or.kr/cms/resource/36/2874836_image2_1.jpg',
+            people_number: 2,
+            start_date: '2023-12-03',
+            end_date: '2023-12-04',
+            representative_name: '홍길동',
+            order_price: 70000,
+          },
+          {
+            order_date: '2023-11-29',
+            accommodation_id: 100,
+            accommodation_name: '저스트슬립호텔 김포구래1호점',
+            accommodation_img:
+              'http://tong.visitkorea.or.kr/cms/resource/83/2874883_image2_1.jpg',
+            people_number: 2,
+            start_date: '2023-12-03',
+            end_date: '2023-12-04',
+            representative_name: '홍길동',
+            order_price: 50000,
+          },
+          {
+            order_date: '2023-11-29',
+            accommodation_id: 99,
+            accommodation_name: '호텔 라르 김포',
+            accommodation_img:
+              'http://tong.visitkorea.or.kr/cms/resource/04/2874904_image2_1.jpg',
+            people_number: 2,
+            start_date: '2023-12-03',
+            end_date: '2023-12-04',
+            representative_name: '홍길동',
+            order_price: 700000,
+          },
+          {
+            order_date: '2023-11-29',
+            accommodation_id: 96,
+            accommodation_name: '루브호텔',
+            accommodation_img:
+              'http://tong.visitkorea.or.kr/cms/resource/91/2877391_image2_1.jpg',
+            people_number: 2,
+            start_date: '2023-12-03',
+            end_date: '2023-12-04',
+            representative_name: '홍길동',
+            order_price: 40000,
+          },
+          {
+            order_date: '2023-11-29',
+            accommodation_id: 95,
+            accommodation_name: '라인플러스호텔',
+            accommodation_img:
+              'http://tong.visitkorea.or.kr/cms/resource/25/2877425_image2_1.jpg',
+            people_number: 2,
+            start_date: '2023-12-03',
+            end_date: '2023-12-04',
+            representative_name: '홍길동',
+            order_price: 60000,
+          },
+        ],
+      ]);
     };
 
     return (
@@ -53,8 +249,8 @@ export const ExampleReservationList: StoryObj = {
           hasMore={true}
           loader={<h4>Loading...</h4>}>
           <section className={styles.reservationItemContainer}>
-            {items.map((_, index) => (
-              <ReservationItem key={index} />
+            {items.map((item, index) => (
+              <ReservationItem key={index} item={item} />
             ))}
           </section>
         </InfiniteScroll>
