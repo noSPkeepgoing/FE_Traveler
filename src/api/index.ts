0e0d7e57b8d884a559c24348d71d0e03fabb76e2
junkue20 <junkue13@gmail.com>
2023-11-30 01:51:17 +0900

    ♻️ Refactor: 엑세스 토큰값 수정

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,12 +1,13 @@
 import { HTTP_BASE_URL } from '@/constants/api';
 import axios from 'axios';
 
+const accessToken = sessionStorage.getItem('access_token');
+
 export const instance = axios.create({
   baseURL: HTTP_BASE_URL,
-  // headers: {
-  //   Authorization:
-  //     'Bearer eyJhbGciOiJIUzUxMiJ9.eyJjbGllbnRfaWQiOiIyIiwiZW1haWwiOiJ0ZXN0QGdtYWlsLmNvbSIsImlzcyI6ImZjX21pbmkiLCJpYXQiOjE3MDExNjM3MzYsImV4cCI6MTcwMTE5OTczNn0.fSDhmD8alzdyXK64obHEp-CSclULpWWnT2IPlH2Cu_k8QPdXcPEGlN3ptoFkJnXFdNjxgJLQQeApLZeVy27tVQ',
-  // },
+  headers: {
+    Authorization: `Bearer ${accessToken}`,
+  },
 });
 
 // interceptor 코드
