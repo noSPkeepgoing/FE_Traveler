7ff26be418f5e9deb47459dd4aa27e728a37e8a2
junkue20 <junkue13@gmail.com>
2023-11-30 01:51:50 +0900

    ✨ Feat: 로그아웃 api 추가

---
+++ src/api/signOut/index.ts
@@ -0,0 +1,7 @@
+import { instance } from '../';
+import { Response } from '../type';
+
+export const SIGN_API = {
+  // 로그아웃 API
+  userSignOut: () => instance.post<Response>('v1/member/logout'),
+};
