9d1bf1018816f9305f1ee5255a677739d4ad7955
junkue20 <junkue13@gmail.com>
2023-11-29 22:34:31 +0900

    ♻️ Refactor: 로그인 유효성 검사코드 추가 및 API 연결코드 수정 완료

--- src/app/(non-header)/sign-in/_components/signInForm/index.tsx
+++ src/app/(non-header)/sign-in/_components/signInForm/index.tsx
@@ -1,22 +1,125 @@
 'use client';
-import React from 'react';
+import React, { FormEvent, useState } from 'react';
 import styles from './signInForm.module.scss';
 import Text from '@/components/atoms/text';
 import Button from '@/components/atoms/button';
 import { TSignIn } from './signInType';
+import Input from '@/components/atoms/input';
+import { SIGNIN_API } from '@/api/signIn/Index';
+import { RESPONSE_CODE } from '@/constants/api';
+import { isAxiosError } from 'axios';
+import { Response } from '@/api/type';
 
 function SignInForm() {
-  // api 전달 함수
-  const signInSubmit = (
-    event: React.FormEvent<HTMLFormElement>,
-    formData: TSignIn,
+  // 각 Input 라벨 메세지용 state
+  const [emailMessage, setEmailMessage] = useState({
+    message: '',
+    error: false,
+  });
+  const [passwordMessage, setPasswordMessage] = useState({
+    message: '',
+    error: false,
+  });
+
+  // 각 상태관리 함수 메세지값 전달용 함수
+  const setMessageAndHideAfterDelay = (
+    setMessage: Function,
+    message: string,
+    error: boolean,
   ) => {
+    setMessage({ message, error });
+    setTimeout(() => setMessage({ message: '', color: 'black' }), 3000); // 3초 후 메시지를 숨기는 용도.
+  };
+
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    // 폼 제출(submit) 이벤트 발생 시 실행할 api가 들어갈 자리
+
+    // form 데이터 Target함수
+    const formData = new FormData(event.currentTarget);
+    const userData: TSignIn = {
+      email: formData.get('email') as string,
+      password: formData.get('password') as string,
+    };
+
+    // 각 form 데이터 유효성 검사 실시
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (!userData.email) {
+      setMessageAndHideAfterDelay(
+        setEmailMessage,
+        '이메일을 입력해주세요.',
+        true,
+      );
+      return;
+    } else if (!emailRegex.test(userData.email)) {
+      setMessageAndHideAfterDelay(
+        setEmailMessage,
+        '올바르지 않은 이메일 형식입니다.',
+        true,
+      );
+      return;
+    } else if (!userData.password) {
+      setMessageAndHideAfterDelay(
+        setPasswordMessage,
+        '비밀번호를 입력해주세요.',
+        true,
+      );
+      return;
+    }
+
+    // 로그인 API 호출
+    try {
+      const response = await SIGNIN_API.userSignIn(userData);
+      const accessToken = response.data.data.access_token;
+      const refreshToken = response.data.data.refresh_token;
+      const userName = response.data.data.name;
+      const userEmail = response.data.data.email;
+
+      // 세션 스토리지에 토큰 값 저장
+      sessionStorage.setItem('accessToken', accessToken);
+      sessionStorage.setItem('refreshToken', refreshToken);
+      sessionStorage.setItem('userName', userName);
+      sessionStorage.setItem('userEmail', userEmail);
+
+      const responseCode = response.data.code;
+
+      switch (responseCode) {
+        case RESPONSE_CODE.SIGNIN_SUCCESS:
+          window.location.href = '/main';
+
+          break;
+      }
+    } catch (error: unknown) {
+      if (isAxiosError<Response>(error)) {
+        if (error.response) {
+          const responseCode = error.response.data.code;
+          switch (responseCode) {
+            case RESPONSE_CODE.INCORRECT_ID:
+              setMessageAndHideAfterDelay(
+                setEmailMessage,
+                '회원 아이디가 존재하지 않습니다.',
+                true,
+              );
+              break;
+            case RESPONSE_CODE.INCORRECT_ID:
+              setMessageAndHideAfterDelay(
+                setPasswordMessage,
+                '비밀번호가 올바르지 않습니다.',
+                true,
+              );
+              break;
+            default:
+              alert('로그인에 실패했습니다. 서버와의 연결을 확인해주세요.');
+              break;
+          }
+        }
+      }
+    }
   };
 
   return (
-    <form action="/v1/member/login" method="post">
+    <form onSubmit={handleSubmit}>
       <div className={styles.form}>
         <div className={styles.info}>
           <Text color="primary" fontSize="xl" fontWeight="medium">
@@ -25,20 +128,48 @@ function SignInForm() {
         </div>
         <div className={styles.inputContainer}>
           <div className={styles.inputBox}>
-            <input type="email" placeholder="이메일" name="email" />
-            <label className={styles.label} htmlFor="">
-              이메일 상태 모시깽이
-            </label>
+            <Input
+              variant="signRegular"
+              type="email"
+              placeholder="이메일"
+              name="email"
+              state={emailMessage.error ? 'invalid' : ''}
+            />
+            <div className={styles.labelArea}>
+              <label className={styles.label} htmlFor="email">
+                <Text
+                  fontSize="xs-3"
+                  fontWeight="normal"
+                  color={emailMessage.error ? 'red100' : 'black'}>
+                  {emailMessage.message || ''}
+                </Text>
+              </label>
+            </div>
           </div>
         </div>
         <div className={styles.inputContainer}>
           <div className={styles.inputBox}>
-            <input type="password" placeholder="비밀번호" name="password" />
-            <label htmlFor="">비밀번호 상태 모시깽이</label>
+            <Input
+              variant="signRegular"
+              type="password"
+              placeholder="비밀번호"
+              name="password"
+              state={passwordMessage.error ? 'invalid' : ''}
+            />
+            <div className={styles.labelArea}>
+              <label htmlFor="password">
+                <Text
+                  fontSize="xs-3"
+                  fontWeight="normal"
+                  color={passwordMessage.error ? 'red100' : 'black'}>
+                  {passwordMessage.message || ''}
+                </Text>
+              </label>
+            </div>
           </div>
         </div>
         <div className={styles.confirm}>
-          <Button size="lg">
+          <Button size="lg" type="submit">
             <Text color="gray100" fontSize="xs-2" fontWeight="medium">
               로그인
             </Text>
