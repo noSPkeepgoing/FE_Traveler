a276d97daf5a57a33dba19d0e899158e6e1f7c51
junkue20 <junkue13@gmail.com>
2023-11-29 22:32:46 +0900

    ♻️ Refactor: 컨벤션에 따른 로그인 api 작성 및 수정

--- src/api/signIn/Index.ts
+++ src/api/signIn/Index.ts
@@ -1,26 +1,9 @@
-import { isAxiosError } from 'axios';
 import { instance } from '../';
-import { Response, ErrorData } from '../type';
-import { TSignInData } from './signInType';
+import { Response } from '../type';
+import { TSignInData, TSignInResponseData } from './signInType';
 
 export const SIGNIN_API = {
-  userSignIn: async (userData: TSignInData) => {
-    try {
-      const response = await instance.post('/v1/member/login', userData);
-      const code = response.data.code;
-      const accessToken = response.data.data.accessToken;
-      const refreshtoken = response.data.data.refreshtoken;
-      // 1. 쿠키에 넣기(2개 다! 원래는 따로.)
-
-      // 2. accessToken 주기에 맞춰서 쿠키에 넣기.
-      return code;
-    } catch (error) {
-      if (isAxiosError<Response<ErrorData>>(error)) {
-        if (error.response) {
-          const code: number = error.response.data.code;
-          return code;
-        }
-      }
-    }
-  },
+  // 로그인 api
+  userSignIn: (userData: TSignInData) =>
+    instance.post<Response<TSignInResponseData>>('v1/member/login', userData),
 };
