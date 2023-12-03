46fb45deb0ae75916759a37c7a111b06f8207087
89882 <pgh123166@naver.com>
2023-12-04 01:26:17 +0900

    📦 Chore: loader 타입 추가

--- src/components/layouts/loader/index.tsx
+++ src/components/layouts/loader/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import styles from './loader.module.scss';
+import { TLoader } from './loaderType';
 
-function Loader({ size = 'lg' }) {
+function Loader({ size = 'lg' }: TLoader) {
   return (
     <div className={`${styles['loader']} ${styles[size]}`}>
       <div className={`${styles['spinner']} ${styles[size]}`}></div>
