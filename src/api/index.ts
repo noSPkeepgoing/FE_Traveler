9cc5afb39c9ffc9eb6ad87b41bd988889370d403
junkue20 <junkue13@gmail.com>
2023-12-05 17:13:03 +0900

    📦 Chore: 불필요 주석 및 코드 제거

--- src/api/index.ts
+++ src/api/index.ts
@@ -19,11 +19,9 @@ export const instance = axios.create({
 instance.interceptors.request.use(
   (config) => {
     if (!isServer) {
-      // 세션 스토리지에서 accessToken 가져오기.
       const accessToken =
         sessionStorage && sessionStorage.getItem('accessToken');
       if (accessToken) {
-        // accessToken이 존재하면 Authorization 헤더 설정.
         config.headers.Authorization = `Bearer ${accessToken}`;
       }
     }
@@ -42,26 +40,12 @@ instance.interceptors.response.use(
   async (error) => {
     const originalRequest = error.config;
 
-    // 여기서는 토큰 만료 상태를 확인하여 재발급 로직을 처리.
     if (error.response?.status === 403 && !originalRequest._retry) {
       originalRequest._retry = true;
 
-      // if (!sessionStorage.getItem('refreshToken')) {
-      //   alert('로그인이 필요한 서비스입니다!');
-      //   return;
-      // }
-
       try {
-        // 토큰 재발급 함수 호출
         const newToken = await TOKEN_API.tokenRefresh();
-        // 토큰 재발급 성공 시 세션 스토리지에 새로운 토큰 저장
         sessionStorage.setItem('accessToken', newToken.data.data.accessToken);
-
-        // 기존 refreshToken값
-
-        // 이전 요청에 새로운 토큰을 적용하여 재시도
-        // originalRequest.headers.Authorization = `Bearer ${newToken.data.data.accessToken}`;
-
         return axios(originalRequest);
       } catch (refreshError) {
         console.error('토큰 재발급 에러: ', refreshError);
--- src/app/(non-header)/sign-in/page.tsx
+++ src/app/(non-header)/sign-in/page.tsx
@@ -1,11 +1,7 @@
 import React from 'react';
-import styles from './signIn.module.scss';
-import Button from '@/components/atoms/button';
-import Text from '@/components/atoms/text';
 import Footer from '@/components/layouts/footer';
 import SignInLogo from './_components/signInLogo';
 import SignInForm from './_components/signInForm';
-
 
 function SignInPage() {
   return (
