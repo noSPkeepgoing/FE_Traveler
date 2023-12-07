e9ab8fc7ca6d784043289746bd285514cf82cbe2
junkue20 <junkue13@gmail.com>
2023-12-07 18:44:27 +0900

    📦 Chore: 불필요한 주석 삭제

--- src/api/refresh-token/index.ts
+++ src/api/refresh-token/index.ts
@@ -3,7 +3,6 @@ import { Response } from '../type';
 import { TTokenResponse } from './refreshTokenType';
 
 export const TOKEN_API = {
-  // 로그아웃 API
   tokenRefresh: () =>
     instance.post<Response<TTokenResponse>>('v1/member/refresh'),
 };
