88e3068432eb0b3170d9148713bfe0d61ed7f018
kyu <qorudrb14@naver.com>
2023-12-07 18:22:47 +0900

    ♻️Refactor:캐러셀 현재보고있는 이미지가 최소,최대일때 이전,다음 버튼 disabled

--- src/app/(header)/(wide-view)/rooms/[id]/_component/carousel/index.tsx
+++ src/app/(header)/(wide-view)/rooms/[id]/_component/carousel/index.tsx
@@ -1,6 +1,6 @@
 'use client';
 import styles from './carousel.module.scss';
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
@@ -11,15 +11,17 @@ function Carousel({ imgs }: TCarousel) {
   const slickRef = useRef<Slider | null>(null);
   const previous = () => slickRef.current?.slickPrev();
   const next = () => slickRef.current?.slickNext();
+  const [currentImage, setCurrentImage] = useState(0);
   const settings = {
     dots: false,
     arrows: false,
     infinite: false,
+    draggable: false,
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
   };
-
+  console.log(imgs.length);
   return (
     <div className={styles.carousel}>
       <Slider {...settings} ref={slickRef}>
@@ -29,11 +31,21 @@ function Carousel({ imgs }: TCarousel) {
           </div>
         ))}
       </Slider>
-      <div onClick={previous}>
-        <GrPrevious className={styles.prevButton} />
+      <div
+        onClick={() => {
+          previous();
+          setCurrentImage((prev) => prev - 1);
+        }}>
+        {currentImage !== 0 && <GrPrevious className={styles.prevButton} />}
       </div>
-      <div onClick={next}>
-        <GrNext className={styles.nextButton} />
+      <div
+        onClick={() => {
+          next();
+          setCurrentImage((prev) => prev + 1);
+        }}>
+        {currentImage !== imgs.length - 1 && (
+          <GrNext className={styles.nextButton} />
+        )}
       </div>
     </div>
   );
