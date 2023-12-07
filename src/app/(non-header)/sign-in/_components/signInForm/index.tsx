bf540173d8971a95ab35ac6256b8ba37f6360b00
junkue20 <junkue13@gmail.com>
2023-12-08 00:45:12 +0900

    📦 Chore: alert 메세지 변경

--- src/app/(non-header)/sign-in/_components/signInForm/index.tsx
+++ src/app/(non-header)/sign-in/_components/signInForm/index.tsx
@@ -118,7 +118,7 @@ function SignInForm() {
               );
               break;
             default:
-              Swal.fire('로그인에 실패했습니다. 관리자에게 문의하세요.');
+              Swal.fire('로그인에 실패했습니다.\n잠시 후 다시 시도하세요.');
               break;
           }
         }
