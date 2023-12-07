9bf4f2338557d2e842835ca0ded16ce86a2ef4a6
junkue20 <junkue13@gmail.com>
2023-12-07 22:24:55 +0900

    ✨ Feat: Sign Form 로고 및 Footer 로고에 main 이동 기능 추가

--- src/app/(non-header)/sign-up/_components/signUpLogo/index.tsx
+++ src/app/(non-header)/sign-up/_components/signUpLogo/index.tsx
@@ -1,13 +1,18 @@
+'use client';
 import React from 'react';
 import styles from './signUpLogo.module.scss';
 import Text from '@/components/atoms/text';
+import { useRouter } from 'next/navigation';
 
 function SignUpLogo() {
+  const router = useRouter();
   return (
     <div className={styles.logo}>
-      <Text color="black" fontSize="xl" fontWeight="thin">
-        Traveler
-      </Text>
+      <div className={styles.textBox} onClick={() => router.push('/main')}>
+        <Text color="black" fontSize="xl" fontWeight="thin">
+          Traveler
+        </Text>
+      </div>
     </div>
   );
 }
--- src/components/layouts/footer/index.tsx
+++ src/components/layouts/footer/index.tsx
@@ -1,16 +1,25 @@
+'use client';
+
 import React from 'react';
 import styles from './footer.module.scss';
 import Text from '@/components/atoms/text';
+import { useRouter } from 'next/navigation';
 
 function Footer() {
+  const router = useRouter();
+
   return (
     <>
       <div className={styles.footer}>
         <div className={styles.footerContainer}>
           <div className={styles.footerChild}>
-            <Text color="black" fontSize="xl" fontWeight="thin">
-              Traveler
-            </Text>
+            <div
+              className={styles.textBox}
+              onClick={() => router.push('/main')}>
+              <Text color="black" fontSize="xl" fontWeight="thin">
+                Traveler
+              </Text>
+            </div>
           </div>
           <div className={styles.footerChild}>
             <Text color="sub" fontSize="xs-5" fontWeight="normal">
