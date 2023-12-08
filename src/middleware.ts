e467ccc9cff3c19934de8ee00884727abe488c67
junkue20 <junkue13@gmail.com>
2023-12-08 18:38:26 +0900

    📦 Chore: 미들웨어 파일 줄띄움 삭제

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,7 +6,6 @@ export async function middleware(request: NextRequest) {
   if (!token) {
     return NextResponse.redirect(new URL('/sign-in', request.url));
   }
-
   return NextResponse.next();
 }
 
