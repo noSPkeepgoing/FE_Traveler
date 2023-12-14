d072734c6512e8d7ce23e08e26e5b78bb1512a68
sunpaaaa <sunpaaaa@gmail.com>
2023-12-14 10:47:09 +0900

    :bug: Fix : 메인페이지 데이터 필드명 변경으로 인한 404 에러 해결

--- src/api/main/index.ts
+++ src/api/main/index.ts
@@ -6,7 +6,7 @@ export const MAIN_API = {
   getAccommodations: (category: number, page?: number) =>
     instance.get<Response<TMainListData>>('/v1/accommodations', {
       params: {
-        location: category,
+        regionIds: category,
         size: 12,
         page: page,
       },
